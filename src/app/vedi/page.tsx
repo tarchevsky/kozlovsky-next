a9959bec58350f5800adff6233599d198eec0951
tarchevsky <i.tarchevsky@gmai.com>
2024-11-17 22:11:44 +0300

    Страница vedi и data к ней + картинки

---
+++ src/app/vedi/page.tsx
@@ -0,0 +1,22 @@
+import type {Metadata} from "next";
+import MapContent from "@/components/mapContent/MapContent";
+import projectsData from "@/data/projectsData";
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+import vediData from "@/data/vediData";
+
+export const metadata: Metadata = {
+    title: 'Проект Vedi',
+}
+
+export default function
+    VediPage() {
+    return (
+        <>
+            <MapContent projectsData={vediData}/>
+            <Header />
+            <MapContent projectsData={projectsData} reverse/>
+            <Footer/>
+        </>
+    )
+}
