74e53a4c9ab2a4f8112f489b30ce9bf5cff4ce06
tarchevsky <i.tarchevsky@gmai.com>
2024-11-18 00:17:54 +0300

    layout - убрал use client и поставил мета, до этого забыл и мета на других страницах был некорректный + глобальная константа с названием сайта

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
-'use client'
-
 import {ReactNode} from "react"
 import './globals.css'
+import {Metadata} from "next";
+import {SITE_NAME} from "@/constants/site.constants";
+
+export const metadata: Metadata = {
+    title: {
+        default: SITE_NAME,
+        template: `%s | ${SITE_NAME}`,
+    },
+}
 
 export default function RootLayout({
                                        children,
